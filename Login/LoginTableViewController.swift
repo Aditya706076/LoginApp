87f6ca07d273deace15fe831348e7b204b68a7db
Aditya <adityaraj8330@gmail.com>
2021-06-24 20:38:04 +0530

    Updated

---
+++ Login/LoginTableViewController.swift
@@ -0,0 +1,31 @@
+//
+//  LoginTableViewController.swift
+//  Login
+//
+//  Created by Aditya on 24/06/21.
+//
+
+import UIKit
+
+class LoginTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func ForgetBtn(_ sender: Any) {
+        print("Forget Password")
+    }
+    
+    @IBAction func LoginBtn(_ sender: Any) {
+        print("Login Successfully")
+    }
+
+    @IBAction func SignBtn(_ sender: Any) {
+    print("New Account")
+    }
+    
+}
+
+
